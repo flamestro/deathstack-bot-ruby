18f911b83d0a6ebd0e5d4ffbe80e0d32bc741632
Ahmet Kilic <19ahmkil99@gmail.com>
2019-03-08 22:50:41 +0100

    Die eingabe wird in einer Variable als Liste gespeichert

--- DeathStacksBot.rb
+++ DeathStacksBot.rb
@@ -1,6 +1,15 @@
-puts "Enter Fen"
-fen = gets.chomp
-puts "Enter Player Char"
+puts     "Enter Fen"
+fen    = gets.chomp
+puts     "Enter Player Char"
 player = gets.chomp
 
-puts fen + " " +player
+
+felder = fen.split("/")
+felder = felder.join(",")
+felder = felder.split(",")
+
+puts felder
+def listMoves 
+	
+end
+
