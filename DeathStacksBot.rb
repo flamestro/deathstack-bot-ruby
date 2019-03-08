ce76a56cfaadea72db41e7b64bd8b87b037445b9
Ahmet Kilic <19ahmkil99@gmail.com>
2019-03-08 23:24:45 +0100

    Fen Spaltung in eine Methode extrahiert

--- DeathStacksBot.rb
+++ DeathStacksBot.rb
@@ -1,14 +1,20 @@
+#Eingabe der fen Not und des Players
 puts     "Enter Fen"
 fen    = gets.chomp
 puts     "Enter Player Char"
 player = gets.chomp
 
+#spalte die fen Notation in eine Collection mit 
+#den stapeln inklusive leere Stapel
+def splitFen(fenNot)
+	felder = fen.split("/")
+	felder = felder.join(",")
+	felder = felder.split(",")
+	return felder
+end
 
-felder = fen.split("/")
-felder = felder.join(",")
-felder = felder.split(",")
 
-puts felder
+
 def listMoves 
 	
 end
