6ee80d2d2e45323f2bcd2d2711bb73d87ab1708a
FlameStro <19ahmkil99@gmail.com>
2019-03-12 18:28:05 +0100

    Move hinzugefügt als class FieldPos hinzugefügt und ein paar Hashes hinzugefügt

--- Spielbrett.rb
+++ Spielbrett.rb
@@ -1,3 +1,23 @@
+$width_height = 6
+    $zeileConv= {
+    1 => '1',
+    2 => '2',
+    3 => '3',
+    4 => '4',
+    5 => '5',
+    6 => '6',
+    nil => 'x'
+    }
+    
+    $spalteConv= {
+    1 => 'a',
+    2 => 'b',
+    3 => 'c',
+    4 => 'd',
+    5 => 'e',
+    6 => 'f',
+    nil => 'x'
+    }
 #simulates a stack on the GameField
 class GameStack 
     attr_accessor :fenFeld , :value , :ownedBy , :indexNr , :zeile , :spalte
@@ -15,11 +35,60 @@ class GameStack
     protected
     #calculates the row
     def zeileAusindexNr
-        (6- ((@indexNr - (@indexNr % 6))/6))
+        temp = @indexNr.dup
+        cnt = 0
+        while temp > $width_height
+        temp -= $width_height
+        cnt+=1
+        end
+        $width_height-cnt
     end
     #calculates the col
     def spalteAusindexNr
-        (@indexNr % 6)
+        temp = @indexNr.dup
+        while temp > $width_height
+        temp -= $width_height
+        end
+        temp
     end
 end
 
+
+
+class FieldPos
+     attr_accessor :zeile , :spalte     
+    def initialize(zeile,spalte)
+        @zeile    = zeile
+        @spalte    = spalte
+    end
+    def same?(other)
+        @zeile == zeile && @spalte == spalte
+    end
+    def to_s
+    if($spalteConv[spalte] == nil || $zeileConv[zeile] == nil )
+        "x"
+    else
+        $spalteConv[spalte]+$zeileConv[zeile]
+    end
+    end
+    
+    
+end
+
+class Move 
+    attr_accessor :startPos , :schritte , :endPos 
+    def initialize(startPos,schritte,endPos)
+        @startPos    = startPos
+        @schritte    = schritte
+        @endPos      = endPos
+    end
+    def same?(other)
+        @startPos == other.startPos && @schritte == other.schritte && @endPos == other.endPos
+    end
+    
+    def to_s 
+        
+        (startPos.to_s)+"-"+(schritte.to_s)+"-"+(endPos.to_s)
+      
+    end
+end
